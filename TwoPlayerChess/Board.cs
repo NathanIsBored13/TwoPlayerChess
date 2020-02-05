d3e1329369405f7394b6d37b8944a74fbf9d782b
Nathan Smith <K036659@eltham-college.org.uk>
2020-02-05 12:55:39 +0000

    Made grid generation and Board classes

---
+++ TwoPlayerChess/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoPlayerChess
+{
+    class Board
+    {
+        Cell[,] grid;
+        public Board(Cell[,] grid)
+        {
+            this.grid = grid;
+        }
+    }
+}
--- TwoPlayerChess/MainWindow.xaml.cs
+++ TwoPlayerChess/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace TwoPlayerChess
     /// </summary>
     public partial class MainWindow : Window
     {
+        int cellSize = 75;
+        Board board;
         public MainWindow()
         {
             InitializeComponent();
+            Cell[,] grid = new Cell[9, 9];
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    Button button = new Button
+                    {
+                        Uid = $"{x.ToString()}:{y.ToString()}",
+                        Width = cellSize,
+                        Height = cellSize,
+                        Margin = new Thickness(1, 1, 1, 1),
+                        Background = (x + y) % 2 == 0? Brushes.GhostWhite : Brushes.DimGray
+                    };
+                    button.Click += GridButtonPressed;
+                    grid[x, y] = new Cell(button);
+                    UniformGrid.Children.Add(button);
+                }
+            }
+            board = new Board(grid);
+        }
+
+        private void GridButtonPressed(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string[] name = button.Uid.Split(':');
+            Console.WriteLine($"{name[0]}, {name[1]}");
         }
     }
 }
